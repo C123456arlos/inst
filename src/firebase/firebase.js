b9537aa8872924e9af5ca3e00bc4f145102ba1c8
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2025-05-08 15:23:09 -0400

    safe

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,20 +4,20 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyC47o87qQiaSFCyQGs-iFnnHAFFcIQPDgg",
-    authDomain: "insta-19bd3.firebaseapp.com",
-    projectId: "insta-19bd3",
-    storageBucket: "insta-19bd3.firebasestorage.app",
-    messagingSenderId: "705075065151",
-    appId: "1:705075065151:web:131f2a7c1edcef329f238a",
-    measurementId: "G-4HS3QSL57Y"
-    // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+    // apiKey: "AIzaSyC47o87qQiaSFCyQGs-iFnnHAFFcIQPDgg",
+    // authDomain: "insta-19bd3.firebaseapp.com",
+    // projectId: "insta-19bd3",
+    // storageBucket: "insta-19bd3.firebasestorage.app",
+    // messagingSenderId: "705075065151",
+    // appId: "1:705075065151:web:131f2a7c1edcef329f238a",
+    // measurementId: "G-4HS3QSL57Y"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
